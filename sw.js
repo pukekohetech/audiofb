fc745370890bed343bb476f36674326476eadd88
pukekohetech <33167310+pukekohetech@users.noreply.github.com>
2026-04-08 13:07:08 +1200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const CACHE_NAME = "audio-feedback-recorder-v2";
 const APP_SHELL = [
   "./",
-  "'./index.html",
+  "./index.html",
   "./audio_feedback_recorder.html",
   "./manifest.webmanifest",
   "./icons/icon-192.png",
